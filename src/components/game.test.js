2bd8143a3e9abb7636bebc3eb323c2d91c651fde
Elias Mason <eliasjmason@gmail.com>
2017-11-14 00:40:17 -0800

    Pretty

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,53 +1,53 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Game from './game';
 
 describe('<Game />', () => {
-    it('Renders without crashing', () => {
-        shallow(<Game />);
+  it('Renders without crashing', () => {
+    shallow(<Game />);
+  });
+
+  it('Can starts a new game', () => {
+    const wrapper = shallow(<Game />);
+    // Mess up the state a bit to simulate an existing game
+    wrapper.setState({
+      guesses: [1, 2, 3, 4],
+      feedback: 'Awesome',
+      correctAnswer: -1 // Negative so different to new game
+    });
+    wrapper.instance().restartGame();
+    expect(wrapper.state('guesses')).toEqual([]);
+    expect(wrapper.state('feedback')).toEqual('Make your guess!');
+    expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
+    expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+  });
+
+  it('Can make guesses', () => {
+    const wrapper = shallow(<Game />);
+    // Set the correctAnswer so we know what we're aiming for
+    wrapper.setState({
+      correctAnswer: 100
     });
 
-    it('Can starts a new game', () => {
-        const wrapper = shallow(<Game />);
-        // Mess up the state a bit to simulate an existing game
-        wrapper.setState({
-            guesses: [1, 2, 3, 4],
-            feedback: 'Awesome',
-            correctAnswer: -1 // Negative so different to new game
-        });
-        wrapper.instance().restartGame();
-        expect(wrapper.state('guesses')).toEqual([]);
-        expect(wrapper.state('feedback')).toEqual('Make your guess!');
-        expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
-        expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
-    });
+    wrapper.instance().makeGuess(25);
+    expect(wrapper.state('guesses')).toEqual([25]);
+    expect(wrapper.state('feedback')).toEqual("You're Ice Cold...");
 
-    it('Can make guesses', () => {
-        const wrapper = shallow(<Game />);
-        // Set the correctAnswer so we know what we're aiming for
-        wrapper.setState({
-            correctAnswer: 100
-        });
+    wrapper.instance().makeGuess(60);
+    expect(wrapper.state('guesses')).toEqual([25, 60]);
+    expect(wrapper.state('feedback')).toEqual("You're Cold...");
 
-        wrapper.instance().makeGuess(25);
-        expect(wrapper.state('guesses')).toEqual([25]);
-        expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
+    wrapper.instance().makeGuess(80);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80]);
+    expect(wrapper.state('feedback')).toEqual("You're Warm.");
 
-        wrapper.instance().makeGuess(60);
-        expect(wrapper.state('guesses')).toEqual([25, 60]);
-        expect(wrapper.state('feedback')).toEqual('You\'re Cold...');
+    wrapper.instance().makeGuess(95);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95]);
+    expect(wrapper.state('feedback')).toEqual("You're Hot!");
 
-        wrapper.instance().makeGuess(80);
-        expect(wrapper.state('guesses')).toEqual([25, 60, 80]);
-        expect(wrapper.state('feedback')).toEqual('You\'re Warm.');
-
-        wrapper.instance().makeGuess(95);
-        expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95]);
-        expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
-
-        wrapper.instance().makeGuess(100);
-        expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95, 100]);
-        expect(wrapper.state('feedback')).toEqual('You got it!');
-    });
+    wrapper.instance().makeGuess(100);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95, 100]);
+    expect(wrapper.state('feedback')).toEqual('You got it!');
+  });
 });
