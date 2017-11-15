bca03ee44b4fb8f6891143607fc6cf6eb5a9e142
Elias Mason <eliasjmason@gmail.com>
2017-11-15 01:35:07 -0800

    Test aural status generation

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -32,22 +32,39 @@ describe('<Game />', () => {
 
     wrapper.instance().makeGuess(25);
     expect(wrapper.state('guesses')).toEqual([25]);
-    expect(wrapper.state('feedback')).toEqual("You're Ice Cold...");
+    expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
 
     wrapper.instance().makeGuess(60);
     expect(wrapper.state('guesses')).toEqual([25, 60]);
-    expect(wrapper.state('feedback')).toEqual("You're Cold...");
+    expect(wrapper.state('feedback')).toEqual('You\'re Cold...');
 
     wrapper.instance().makeGuess(80);
     expect(wrapper.state('guesses')).toEqual([25, 60, 80]);
-    expect(wrapper.state('feedback')).toEqual("You're Warm.");
+    expect(wrapper.state('feedback')).toEqual('You\'re Warm.');
 
     wrapper.instance().makeGuess(95);
     expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95]);
-    expect(wrapper.state('feedback')).toEqual("You're Hot!");
+    expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
 
     wrapper.instance().makeGuess(100);
     expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95, 100]);
     expect(wrapper.state('feedback')).toEqual('You got it!');
+  });
+
+  it('Can generate aural updates', () => {
+    const wrapper = shallow(<Game />);
+
+    wrapper.setState({
+      correctAnswer: 100
+    });
+
+    wrapper.instance().makeGuess(25);
+    wrapper.instance().makeGuess(3);
+    wrapper.instance().makeGuess(90);
+
+    wrapper.instance().generateAuralUpdate();
+
+    expect(wrapper.state('auralStatus')).toEqual('Here\'s the status of the game right now: You\'re Warm. You\'ve made 3 guesses. In order of most- to least-recent, they are: 90, 3, 25');
+
   });
 });
