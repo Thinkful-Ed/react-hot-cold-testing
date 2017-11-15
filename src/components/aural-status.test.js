b285906f400987234b8446ec98467dd7c3985887
Elias Mason <eliasjmason@gmail.com>
2017-11-15 01:43:07 -0800

    Add smoke tests for new components

---
+++ src/components/aural-status.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import AuralStatus from './aural-status';
+
+describe('<AuralStatus />', () => {
+  it('Renders without crashing', () => {
+    shallow(<AuralStatus />);
+  });
+});
