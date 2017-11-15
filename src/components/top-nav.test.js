c76f963cc4b7207e7703bd0c4581b1e5930d6170
Elias Mason <eliasjmason@gmail.com>
2017-11-15 01:25:43 -0800

    Remove references to info modal

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
 import TopNav from './top-nav';
 
 describe('<TopNav />', () => {
-    it('Renders without crashing', () => {
-        shallow(<TopNav />);
-    });
+  it('Renders without crashing', () => {
+    shallow(<TopNav />);
+  });
 
-    it('Should call onNewGame when new game is clicked', () => {
-        const callback = jest.fn();
-        const wrapper = shallow(<TopNav onNewGame={callback} />);
-        const link = wrapper.find('.new');
-        link.simulate('click', {
-            preventDefault() {}
-        });
-        expect(callback).toHaveBeenCalled();
+  it('Should call onNewGame when new game is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const link = wrapper.find('.new');
+    link.simulate('click', {
+      preventDefault() {}
     });
-
-    it('Should call onInfo when info is clicked', () => {
-        const callback = jest.fn();
-        const wrapper = shallow(<TopNav onInfo={callback} />);
-        const link = wrapper.find('.what');
-        link.simulate('click', {
-            preventDefault() {}
-        });
-        expect(callback).toHaveBeenCalled();
-    });
+    expect(callback).toHaveBeenCalled();
+  });
 });
